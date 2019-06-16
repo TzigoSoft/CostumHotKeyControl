b3d5412ef40f6b403df9c7b11939fe419c35d7dd
Tzigo <51293140+TzigoSoft@users.noreply.github.com>
2019-06-16 19:58:17 +0200

    Demo Control

---
+++ CustomHotkeyControl Demo/Demo/CustomHotkeyControl.Designer.cs
@@ -0,0 +1,42 @@
+﻿namespace TzigoSoft
+{
+    partial class CustomHotkeyControl
+    {
+        /// <summary> 
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Komponenten-Designer generierter Code
+
+        /// <summary> 
+        /// Erforderliche Methode für die Designerunterstützung. 
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Name = "UserHotkey";
+            this.Size = new System.Drawing.Size(140, 20);
+            this.ResumeLayout(false);
+            this.Text = "--";
+            this.Multiline = false;
+            this.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.BackColor = System.Drawing.Color.White;
+        }
+
+        #endregion
+    }
+}
