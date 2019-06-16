bbaa26cbd35e3dc9fe89b3f7d4e8cb48c1e88f24
Tzigo <51293140+TzigoSoft@users.noreply.github.com>
2019-06-16 20:49:28 +0200

    Demo

---
+++ CustomHotkeyControl Demo/Demo/Demo.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace TzigoSoft
+{
+    partial class Demo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ResetButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SavecheckBox = new System.Windows.Forms.CheckBox();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.customHotkeyControl1 = new TzigoSoft.CustomHotkeyControl();
+            this.SuspendLayout();
+            // 
+            // ResetButton
+            // 
+            this.ResetButton.Location = new System.Drawing.Point(221, 66);
+            this.ResetButton.Name = "ResetButton";
+            this.ResetButton.Size = new System.Drawing.Size(75, 23);
+            this.ResetButton.TabIndex = 1;
+            this.ResetButton.Text = "Reset";
+            this.ResetButton.UseVisualStyleBackColor = true;
+            this.ResetButton.Click += new System.EventHandler(this.ResetButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(42, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(124, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Current Hotkey is: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(195, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // SavecheckBox
+            // 
+            this.SavecheckBox.AutoSize = true;
+            this.SavecheckBox.Location = new System.Drawing.Point(45, 112);
+            this.SavecheckBox.Name = "SavecheckBox";
+            this.SavecheckBox.Size = new System.Drawing.Size(213, 17);
+            this.SavecheckBox.TabIndex = 4;
+            this.SavecheckBox.Text = "Save UserHotkey for usage after restart";
+            this.SavecheckBox.UseVisualStyleBackColor = true;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("Palatino Linotype", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel1.Location = new System.Drawing.Point(125, 143);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(75, 16);
+            this.linkLabel1.TabIndex = 13;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "(c) TzigoSoft";
+            // 
+            // customHotkeyControl1
+            // 
+            this.customHotkeyControl1.BackColor = System.Drawing.Color.White;
+            this.customHotkeyControl1.Location = new System.Drawing.Point(45, 68);
+            this.customHotkeyControl1.Name = "customHotkeyControl1";
+            this.customHotkeyControl1.ShortcutsEnabled = false;
+            this.customHotkeyControl1.Size = new System.Drawing.Size(140, 20);
+            this.customHotkeyControl1.TabIndex = 0;
+            this.customHotkeyControl1.Text = "---";
+            this.customHotkeyControl1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.customHotkeyControl1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.CustomHotkeyControl1_KeyUp);
+            this.customHotkeyControl1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.RightClickReset);
+            // 
+            // Demo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(338, 182);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.SavecheckBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ResetButton);
+            this.Controls.Add(this.customHotkeyControl1);
+            this.Name = "Demo";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "UserHotkey Demo";
+            this.Load += new System.EventHandler(this.Demo_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private CustomHotkeyControl customHotkeyControl1;
+        private System.Windows.Forms.Button ResetButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox SavecheckBox;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
---
+++ CustomHotkeyControl Demo/Demo/Demo.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace TzigoSoft
+{
+    public partial class Demo : Form
+    {
+        #region variables
+
+        //required constants und importfunctions
+        enum KeyModifier { None = 0, Alt = 1, Control = 2, Shift = 4, }
+        [DllImport("user32.dll")]
+        private static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vk);
+        [DllImport("user32.dll")]
+        private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        private static string MainPath = Application.StartupPath;
+        #endregion
+
+        public Demo()
+        {
+            InitializeComponent();
+
+            if (File.Exists(Path.Combine(MainPath, "userconfig.ini")))
+            {
+                //if the Hotkey was saved before exiting the Program, it can be loaded here
+                try
+                {
+                    //transmission of the saved string to the register function of the UserHotKeyControls
+                    customHotkeyControl1.LoadHotKey(File.ReadAllText(Path.Combine(MainPath, "userconfig.ini")));
+
+                    //Setting the Hotkey with the returned variables from CustomHotkeyControl
+                    RegisterHotKey(this.Handle, 0, CustomHotkeyControl.A + CustomHotkeyControl.C + CustomHotkeyControl.S, CustomHotkeyControl.K.GetHashCode());
+                    label2.Text = CustomHotkeyControl.Display;
+                    SavecheckBox.Checked = true;
+
+                    //locks the CustomHotkeyControl against unintentional changes
+                    customHotkeyControl1.locked = true;
+                }
+
+                //in an error occurs, the defaulthotkey is loaded
+                catch
+                {
+                    RegisterHotKey(this.Handle, 0, 2, Keys.F1.GetHashCode());
+                    customHotkeyControl1.Text = "Control + F1";
+                    label2.Text = "Control" + " + " + "F1";
+
+                    //locks the CustomHotkeyControl against unintentional changes
+                    customHotkeyControl1.locked = true;
+                }
+            }
+
+            //otherwise a defaulthotkey can be loaded
+            else
+            {
+                RegisterHotKey(this.Handle, 0, 2, Keys.F1.GetHashCode());
+                customHotkeyControl1.Text = "Control + F1";
+                label2.Text = "Control" + " + " + "F1";
+
+                //locks the CustomHotkeyControl against unintentional changes
+                customHotkeyControl1.locked = true;
+            }
+        }
+
+        private void Demo_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        //hotkey selection and registration
+        private void CustomHotkeyControl1_KeyUp(object sender, KeyEventArgs e)
+        {
+            //Querying if the hotkey is valid
+            if (CustomHotkeyControl.register)
+            {
+                UnregisterHotKey(this.Handle, 0);
+
+                //Setting the Hotkey with the returned variables from CustomHotkeyControl
+                RegisterHotKey(this.Handle, 0, CustomHotkeyControl.A + CustomHotkeyControl.C + CustomHotkeyControl.S, CustomHotkeyControl.K.GetHashCode());
+
+                //CustomHotkeyControl.Display returns the hotkey as a string
+                label2.Text = CustomHotkeyControl.Display;
+
+                //locks the CustomHotkeyControl against unintentional changes
+                customHotkeyControl1.locked = true;
+
+                //here the hotkey canbe saved for usage after a programm restart
+                if (SavecheckBox.Checked == true)
+                {
+                    //CustomHotkeyControl.SaveHotKey returns the hotkey as a string
+                    File.WriteAllText(Path.Combine(MainPath, "userconfig.ini"), CustomHotkeyControl.SaveHotKey);
+                }
+            }
+        }
+
+        //resetting the hotkey per Buttonclick
+        private void ResetButton_Click(object sender, EventArgs e)
+        {
+            //hotkey gets unregisterd
+            UnregisterHotKey(this.Handle, 0);
+
+            //reset of the CustomHotkeyControl 
+            customHotkeyControl1.Reset();
+            label2.Text = customHotkeyControl1.Text;
+
+            //makes the saved varables invalid
+            CustomHotkeyControl.register = false;
+            if (SavecheckBox.Checked == true)
+            {
+
+                //Reset hotkey to default if it should be saved
+                File.WriteAllText(Path.Combine(MainPath, "userconfig.ini"), CustomHotkeyControl.SaveHotKey);
+            }
+
+            MessageBox.Show("Hotkey is unregistered");
+        }
+
+        //or resetting the hotkey per rightclick
+        private void RightClickReset(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                //checks if the CustomHotkeyControl is locked
+                //otherwise right clicks on the CustomHotkeyControl are ignored to block the contextmenu of windows
+                if (customHotkeyControl1.locked == true)
+                {
+                    //Hotkey gets unregisterd
+                    UnregisterHotKey(this.Handle, 0);
+
+                    //resets the CustomHotkeyControl
+                    customHotkeyControl1.Reset();
+                    label2.Text = customHotkeyControl1.Text;
+
+                    //makes the saved varables invalid
+                    CustomHotkeyControl.register = false;
+                    if (SavecheckBox.Checked == true)
+                    {
+                        //Reset hotkey to default if it should be saved
+                        File.WriteAllText(Path.Combine(MainPath, "userconfig.ini"), "Control + F1");
+                    }
+
+                    MessageBox.Show("Hotkey is unregistered");
+                }
+            }
+        }
+
+        //Monitor the set hotkey for further use
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+
+            if (m.Msg == 0x0312)
+            {
+                Keys key = (Keys)(((int)m.LParam >> 16) & 0xFFFF);
+                KeyModifier modifier = (KeyModifier)((int)m.LParam & 0xFFFF);
+                int id = m.WParam.ToInt32();
+                if (id == 0)
+                {
+                    MessageBox.Show("Hotkey is registered and you can do any work with this.");
+                }
+            }
+        }
+
+        private void LinkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            linkLabel1.LinkVisited = true;
+            System.Diagnostics.Process.Start("https://www.tzigosoft.de/");
+        }
+    }
+}
